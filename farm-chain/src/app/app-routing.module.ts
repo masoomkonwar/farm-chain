01f702012ac0732b4ae9d8298652e3d530f0a181
Masoom <msmkonwar@gmail.com>
2020-10-31 17:44:38 +0530

    third

--- farm-chain/src/app/app-routing.module.ts
+++ farm-chain/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CertifiedComponent } from './components/certified/certified.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+ {path:'home',component:HomeComponent},
+ {path:'certified',component:CertifiedComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- farm-chain/src/app/app.module.ts
+++ farm-chain/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { CertifiedComponent } from './components/certified/certified.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CertifiedComponent
   ],
   imports: [
     BrowserModule,
